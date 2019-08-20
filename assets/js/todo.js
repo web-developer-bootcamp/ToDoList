635725f587c6792840a0487463bebd8906b8d518
Yintong <zhouyintong96@gmail.com>
2019-08-20 10:55:25 +0200

    add delete/insert

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,8 +1,21 @@
-$("li").click(function () {
+//click on todo to line through
+$("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
 });
 
 //click on X to delete todo
-$("span").click(function () {
-    alert("X clicked")
+$("ul").on("click", "span", function (e) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    e.stopPropagation();
+});
+
+//insert value to todo list
+$("input[type='text']").on("keypress", function (e) {
+    if (e.which === 13) {
+        var ToDoText = $(this).val();
+        $("ul").append("<li><span>X</span> " + ToDoText + "</li>");
+        $(this).val("");
+    }
 });
